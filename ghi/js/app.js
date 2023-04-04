c8dbb590095f5a4f58496f88f7d668eb3c2737a1
Italiz <italizv@gmail.com>
2023-04-04 14:41:37 -0400

    Implement fetch

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,20 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
+  const url = 'http://localhost:8000/api/conferences/';
+  // add error code to handle future errors
+  try {
+    // fetch function returns a Promise
+    const response = await fetch(url);
 
-
+    if (!response.ok) {
+      // what to do when the response is bad
+      console.log('response is bad')
+    } else {
+      // want the value the Promise will turn into after await
+      const data = await response.json();
+      console.log(data)
+    }
+  } catch (e) {
+    // what to do if an error is raised
+    console.error('There is a problem:', error)
+  }
 });
