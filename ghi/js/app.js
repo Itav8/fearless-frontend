a632b214bd20e536c596ee7b3a7b829616521b49
Italiz <italizv@gmail.com>
2023-04-04 15:57:49 -0400

    Added conference name and description to HTML

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -6,15 +6,29 @@ window.addEventListener('DOMContentLoaded', async () => {
     const response = await fetch(url);
 
     if (!response.ok) {
-      // what to do when the response is bad
-      console.log('response is bad')
+      console.log('Something is not okay')
     } else {
       // want the value the Promise will turn into after await
       const data = await response.json();
-      console.log(data)
+
+      const conference = data.conferences[0];
+      // selects a tag that matches what's being passed in querySelector
+      const nameTag = document.querySelector('.card-title');
+      nameTag.innerHTML = conference.name;
+
+      const detailUrl = `http://localhost:8000${conference.href}`;
+      const detailResponse = await fetch(detailUrl);
+
+      if (detailResponse.ok) {
+        const details = await detailResponse.json();
+        // get the description out of the data
+        const description = details.conference.description;
+        const descriptionTag = document.querySelector('.card-text');
+        descriptionTag.innerHTML = description
+      }
+
     }
   } catch (e) {
-    // what to do if an error is raised
     console.error('There is a problem:', error)
   }
 });
