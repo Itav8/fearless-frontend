91ec1ffd5b6a436c51b3f342f814cf6652c94b29
Italiz <italizv@gmail.com>
2023-04-05 16:23:49 -0400

    Submit data to API

--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -1,5 +1,5 @@
-window.addEventListener('DOMContentLoaded', async () => {
-  const url = 'http://localhost:8000/api/states/';
+window.addEventListener("DOMContentLoaded", async () => {
+  const url = "http://localhost:8000/api/states/";
 
   try {
     const response = await fetch(url);
@@ -9,11 +9,11 @@ window.addEventListener('DOMContentLoaded', async () => {
     } else {
       const data = await response.json();
       // Get the select tag element by its id 'state'
-      const selectTag = document.getElementById('state')
+      const selectTag = document.getElementById("state");
       // For each state in the states property of the data
       for (let state of data.states) {
         // Create an 'option' element
-        const option = document.createElement('option')
+        const option = document.createElement("option");
         // Set the '.value' property of the option element to the
         // state's abbreviation
         option.value = state.abbreviation;
@@ -23,6 +23,30 @@ window.addEventListener('DOMContentLoaded', async () => {
         // Append the option element as a child of the select tag
         selectTag.appendChild(option);
       }
+
+      const formTag = document.getElementById("create-location-form");
+      formTag.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(formTag);
+        const json = JSON.stringify(Object.fromEntries(formData));
+
+        const locationUrl = "http://localhost:8000/api/locations/";
+        const fetchConfig = {
+          method: "post",
+          body: json,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+
+        const newResponse = await fetch(locationUrl, fetchConfig);
+
+        if (newResponse.ok) {
+          formTag.reset();
+          const newLocation = await newResponse.json();
+        }
+      });
     }
   } catch (e) {
     console.log("Problem is:", e);
