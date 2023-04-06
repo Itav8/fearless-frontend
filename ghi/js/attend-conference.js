d014b89d0151c6e72e3a1046e63876eeb96a573f
Italiz <italizv@gmail.com>
2023-04-06 15:35:20 -0400

    Fixed CORS error

--- ghi/js/attend-conference.js
+++ ghi/js/attend-conference.js
@@ -7,9 +7,10 @@ window.addEventListener("DOMContentLoaded", async () => {
 
     if (response.ok) {
       const data = await response.json();
-
+      console.log(data);
       for (let conference of data.conferences) {
         const option = document.createElement("option");
+        // option.value to href
         option.value = conference.href;
         option.innerHTML = conference.name;
         selectTag.appendChild(option);
@@ -22,6 +23,32 @@ window.addEventListener("DOMContentLoaded", async () => {
       loadingIconTag.classList.add("d-none");
       // Here, remove the 'd-none' class from the select tag
       selectTag.classList.remove("d-none");
+
+      const formTag = document.getElementById("create-attendee-form");
+      formTag.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(formTag);
+        const json = JSON.stringify(Object.fromEntries(formData));
+
+        const attendeeUrl = "http://localhost:8001/api/attendees/";
+        const fetchConfig = {
+          method: "post",
+          body: json,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+
+        const newResponse = await fetch(attendeeUrl, fetchConfig);
+
+        if (newResponse.ok) {
+          formTag.reset()
+          const success = document.getElementById("success-message");
+          success.classList.remove("d-none");
+          formTag.classList.add("d-none");
+        }
+      });
     } else {
       throw Error("Failure to se ");
     }
